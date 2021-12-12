062dab0694de2b740df7088335c57d5f5fe2c106
Anton <m@antonz.org>
2021-12-12 23:12:08 +0300

    fix: fileio tests

--- test/fileio.sql
+++ test/fileio.sql
@@ -4,7 +4,7 @@ with data as (
   select name || ',' || mode as value from fsdir('LICENSE')
 )
 select '01', value = 'LICENSE,33188' from data;
-select '02', count(*) > 10 from fsdir('test');
+select '02', count(*) >= 10 from fsdir('test');
 select '03', writefile('hello.txt', 'hello world') = 11;
 select '04', typeof(readfile('hello.txt')) = 'blob';
 select '05', length(readfile('hello.txt')) = 11;
