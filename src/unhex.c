cde02484c9bfbabafdbe9909d03c72b648207e1a
Anton Zhiyanov <m@antonz.ru>
2021-12-20 20:48:11 +0300

    impr: unhex - newer version

--- src/unhex.c
+++ src/unhex.c
@@ -5,6 +5,7 @@
 // Decodes the arg which must be an even number of hexidecimal characters
 // into a blob and returns the blob
 
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,30 +13,38 @@
 SQLITE_EXTENSION_INIT1
 
 static void sqlite3_unhex(sqlite3_context* context, int argc, sqlite3_value** argv) {
-    long olength = sqlite3_value_bytes(argv[0]);
-    long length;
     unsigned char* data = (unsigned char*)sqlite3_value_text(argv[0]);
     unsigned char* blob;
     unsigned char* stuff;
+    int olength = sqlite3_value_bytes(argv[0]);
+    int length;
     unsigned char buffer[4] = {0};
 
     if ((olength % 2 != 0) || (olength < 2)) {
         return;
     }
-
-    blob = malloc(length / 2);
+    blob = sqlite3_malloc(olength / 2);
+    if (blob == 0) {
+        sqlite3_result_error_nomem(context);
+        return;
+    }
     stuff = blob;
     length = olength;
-
     while (length > 0) {
         memcpy(buffer, data, 2);
+        if ((!isxdigit(buffer[0])) || (!isxdigit(buffer[1]))) {
+            break;
+        }
         *stuff = (unsigned char)strtol((const char*)buffer, NULL, 16);
         stuff++;
         data += 2;
         length -= 2;
     }
-    sqlite3_result_blob(context, blob, olength / 2, SQLITE_TRANSIENT);
-    free(blob);
+    if (length == 0) {
+        sqlite3_result_blob(context, blob, olength / 2, sqlite3_free);
+    } else {
+        sqlite3_free(blob);
+    }
 }
 
 #ifdef _WIN32
