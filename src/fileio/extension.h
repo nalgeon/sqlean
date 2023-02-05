670146eb86e2b1c40503e684c85d50c846150c02
Anton <m@antonz.org>
2023-02-05 22:56:46 +0500

    build: fileio - build on windows

--- src/fileio/extension.h
+++ src/fileio/extension.h
@@ -6,21 +6,6 @@
 
 #include "../sqlite3ext.h"
 
-#if defined(FILEIO_WIN32_DLL) && (defined(_WIN32) || defined(WIN32))
-#/* To allow a standalone DLL, use this next replacement function: */
-#undef sqlite3_win32_utf8_to_unicode
-#define sqlite3_win32_utf8_to_unicode utf8_to_utf16
-#
-LPWSTR utf8_to_utf16(const char* z) {
-    int nAllot = MultiByteToWideChar(CP_UTF8, 0, z, -1, NULL, 0);
-    LPWSTR rv = sqlite3_malloc(nAllot * sizeof(WCHAR));
-    if (rv != 0 && 0 < MultiByteToWideChar(CP_UTF8, 0, z, -1, rv, nAllot))
-        return rv;
-    sqlite3_free(rv);
-    return 0;
-}
-#endif
-
 int fileioscalar_init(sqlite3* db);
 int lsdir_init(sqlite3* db);
 int scanfile_init(sqlite3* db);
