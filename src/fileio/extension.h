6e64e3839a7590d82eb93238c65c4867be8c6b70
Anton <m@antonz.org>
2023-06-08 11:08:02 +0500

    refactor: prepare fileio extension

--- src/fileio/extension.h
+++ src/fileio/extension.h
@@ -1,6 +1,8 @@
 // Copyright (c) 2023 Anton Zhiyanov, MIT License
 // https://github.com/nalgeon/sqlean
 
+// Read and write files in SQLite.
+
 #ifndef FILEIO_EXTENSION_H
 #define FILEIO_EXTENSION_H
 
@@ -9,5 +11,6 @@
 int fileio_ls_init(sqlite3* db);
 int fileio_scalar_init(sqlite3* db);
 int fileio_scan_init(sqlite3* db);
+int fileio_init(sqlite3* db);
 
 #endif /* FILEIO_EXTENSION_H */
--- src/sqlite3-fileio.c
+++ src/sqlite3-fileio.c
@@ -4,16 +4,7 @@
 // Modified by Anton Zhiyanov, MIT License
 // https://github.com/nalgeon/sqlean/
 
-/*
- * This SQLite extension implements SQL functions
- * for reading, writing and listing files and folders.
- *
- *
- * Notes on building the extension for Windows:
- * Unless linked statically with the SQLite library, a preprocessor
- * symbol, FILEIO_WIN32_DLL, must be #define'd to create a stand-alone
- * DLL form of this extension for WIN32. See its use below for details.
- */
+// Read and write files in SQLite.
 
 #include "sqlite3ext.h"
 SQLITE_EXTENSION_INIT1
@@ -26,8 +17,5 @@ __declspec(dllexport)
     int sqlite3_fileio_init(sqlite3* db, char** errmsg_ptr, const sqlite3_api_routines* api) {
     (void)errmsg_ptr;
     SQLITE_EXTENSION_INIT2(api);
-    fileio_scalar_init(db);
-    fileio_ls_init(db);
-    fileio_scan_init(db);
-    return SQLITE_OK;
+    return fileio_init(db);
 }
